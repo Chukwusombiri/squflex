a785008d098e088556c554e0e8915761fa8c1436
Chisom Okwuosa <okwuosachisom8@gmail.com>
2025-04-14 22:54:56 +0100

    jdhgha

--- resources/views/components/cia.blade.php
+++ resources/views/components/cia.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full h-full flex flex-col gap-2">
     <img src="{{asset('/images/landing/team.jpg')}}" alt="Team photo" class="w-full h-[60vh] flex-grow rounded-xl object-contain">
-    <p class="flex-shrink-0 text-sm font-light uppercase mt-4 tracking-widest text-center"><span class="font-bold">Liam Thompson</span>, ADVISOR</p>
-    <p class="flex-shrink-0 text-sm font-light uppercase mb-2 tracking-widest text-center">CIM® (CHARTERED INVESTMENT MANAGER)</p>
+    <p class="flex-shrink-0 text-sm font-light uppercase tracking-widest text-center"><span class="font-bold">Liam Thompson</span>, ADVISOR</p>
+    <p class="flex-shrink-0 text-sm font-light uppercase tracking-widest text-center">CIM® (CHARTERED INVESTMENT MANAGER)</p>
 </div>    
