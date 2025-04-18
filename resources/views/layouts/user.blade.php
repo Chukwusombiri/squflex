a2456cd055ce3e2dbfdd1d30666b0c103b762768
Chisom Okwuosa <okwuosachisom8@gmail.com>
2025-04-18 18:30:37 +0100

    jhhjsk

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -49,6 +49,18 @@
             background-image: url('/images/auth/page-header.jpg') !important;
         }
     </style>
+    <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = '0ecc76141a5656bf86d6a25052c793fe82b671a3';
+    window.smartsupp||(function(d) {
+      var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+      s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+      c.type='text/javascript';c.charset='utf-8';c.async=true;
+      c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </head>
 
 <body
